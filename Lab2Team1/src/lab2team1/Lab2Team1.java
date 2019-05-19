91bf4aec5f57e5cd4793e5195c3eed10daf10dea
Andrea <Andrea@10.1.10.33>
2019-05-19 14:56:03 -0400

    Created Repository for Lab2 Group assignment.

---
+++ Lab2Team1/src/lab2team1/Lab2Team1.java
@@ -0,0 +1,21 @@
+
+package lab2team1;
+
+/* 
+Project:
+Purpose Details:
+Course:
+Author:
+Date Developed:
+Last Date Changed:
+Revision:
+*/
+
+public class Lab2Team1 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
