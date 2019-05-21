b9f9d5f88329e6f808a444ace169f9cad66bfba4
milesforsmiles <amm6798@psu.edu>
2019-05-21 18:26:56 -0400

    Demonstration commit

--- Lab2Team1/src/lab2team1/Lab2Team1.java
+++ Lab2Team1/src/lab2team1/Lab2Team1.java
@@ -15,7 +15,7 @@ public class Lab2Team1 {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Look I made a change!");
     }
     
 }
