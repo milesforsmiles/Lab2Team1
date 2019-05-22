aaed55fc72be0ba0174b0bf48671a09b9b4847ca
milesforsmiles <amm6798@psu.edu>
2019-05-21 20:22:50 -0400

    Added in line comment for explanation

--- Lab2Team1/src/lab2team1/Lab2Team1.java
+++ Lab2Team1/src/lab2team1/Lab2Team1.java
@@ -2,20 +2,31 @@
 package lab2team1;
 
 /* 
-Project:
-Purpose Details:
-Course:
-Author:
-Date Developed:
+Project: Deadlock
+Purpose Details: Create an application that makes an
+intentional deadlock between threads upon runtime.
+Course: IST 411
+Author: Team 3
+Date Developed: 5/21/19
 Last Date Changed:
 Revision:
 */
 
 public class Lab2Team1 {
 
+    /*
+    Threads allow multiple processes to run simultaneously.
+    They execute independently of each other and are good 
+    for helping your program to multitask. 
+    This program causes a deadlock where a thread becomes locked
+    because it's waiting for resources from another thread.
+    
+    */
 
     public static void main(String[] args) {
-        System.out.println("Look I made a change!");
+        System.out.println("Look I made a Thread!");
+        
+        Thread t1 = new Thread();
     }
     
 }
